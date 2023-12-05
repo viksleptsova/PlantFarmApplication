27c4a02b77b4e6944a42571804f660e69a9dd629
victoriia sleptsova <vika.sleptsova.02@mail.ru>
2023-12-05 23:24:32 +0600

    some fixes

--- app/src/main/java/com/example/plantfarmapplication/model/services/EventPlantService.kt
+++ app/src/main/java/com/example/plantfarmapplication/model/services/EventPlantService.kt
@@ -7,13 +7,14 @@ import com.example.plantfarmapplication.model.objects.Plant
 
 class EventPlantService: IEventPlantService {
     override val fieldsEventMap: Map<Int, List<Event>>
-        get() = mapOf(App.fieldsService.fields[0].id to eventList, App.fieldsService.fields[1].id to eventList1)
+        get() = mapOf(App.fieldsService.fields[0].id to eventList, App.fieldsService.fields[1].id to eventList1, App.fieldsService.fields[4].id to eventList2)
     override val fieldsPlantMap: Map<Int, List<Plant>>
-        get() = mapOf(App.fieldsService.fields[0].id to plantList, App.fieldsService.fields[1].id to plantList1)
+        get() = mapOf(App.fieldsService.fields[0].id to plantList, App.fieldsService.fields[1].id to plantList1,  App.fieldsService.fields[4].id to plantList2)
 
     private var eventList: List<Event> = listOf(Event(1, "Прополка", "16.10.23"),
         Event(2, "Обработка фунгицидом", "16.10.23")
     )
+
     private var plantList: List<Plant> = listOf(
         Plant(1, 1, "Черемуха", 15),
         Plant(2, 2, "Барбарис", 70),
@@ -27,6 +28,15 @@ class EventPlantService: IEventPlantService {
         Plant(1, 1, "Черемуха", 20),
         Plant(2, 2,"Рябина", 20),
         )
+
+    private var eventList2: List<Event> = listOf(Event(1, "Прополка", "16.10.23"),
+        Event(2, "Обработка фунгицидом", "16.10.23")
+    )
+    private var plantList2: List<Plant> = listOf(
+        Plant(2, 2, "Барбарис", 15),
+        Plant(2, 2, "Кустарник", 15)
+
+    )
 
 
 }
